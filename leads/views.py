d90ece9b7fc89093ca43ba4c30f7d15fdbbd6d9a
barczakmichal <barczakmichal@gmail.com>
2022-02-03 16:48:04 +0100

    views test (import from previous project)

--- leads/views.py
+++ leads/views.py
@@ -1,42 +1,39 @@
-# from datetime import datetime
-# from django.http import HttpResponse, HttpResponseNotFound, Http404
-# from django.shortcuts import render, redirect
-# from django.views import View
-# from .models import Client as Client
+from datetime import datetime
+import email
+from django.http import HttpResponse, HttpResponseNotFound, Http404
+from django.shortcuts import render, redirect
+from django.views import View
+from .models import Client as Client
 
-# class ClientAdd(View):
+class ClientAdd(View):
 
-#     def get(self, request):
-#         return render(request, 'add_client.html')
+    def get(self, request):
+        return render(request, 'add_client.html')
 
-#     def post(self, request):
-#         short_name = request.POST.get('short_name')
-#         full_name = request.POST.get('full_name')
-#         city = request.POST.get('city')
-#         street = request.POST.get('street')
-#         tax_number = request.POST.get('tax_number')
-#         industry = request.POST.get('industry')
-#         website = request.POST.get('website')
-#         source_lead = request.POST.get('source_lead')
-#         status = request.POST.get('status')
+    def post(self, request):
+        short_name = request.POST.get('short_name')
+        full_name = request.POST.get('full_name')
+        city = request.POST.get('city')
+        website = request.POST.get('website')
+        tax_number = request.POST.get('tax_number')
+        email = request.POST.get('email')
+        status = request.POST.get('status')
 
-#         Client.objects.create(short_name=short_name,
-#                              full_name=full_name,
-#                              city=city,
-#                              street=street,
-#                              tax_number=tax_number,
-#                              industry=industry,
-#                              website=website,
-#                              source_lead=source_lead,
-#                              status=status)
+        Client.objects.create(short_name=short_name,
+                             full_name=full_name,
+                             city=city,
+                             website=website,
+                             tax_number=tax_number,
+                             email=email,
+                             status=status)
 
-#         context = {'message':'Dodano do bazy'}
+        context = {'message':'Dodano do bazy'}
 
-#         return render(request, "list_client_code.html", context)
+        return render(request, "list_client_code.html", context)
 
-# class ClientList(View):
+class ClientList(View):
 
-#     def get(self, request):
-#         client_list = Client.objects.all()
-#         print(client_list)
-#         return render(request, 'list_client.html', context={'client_list': client_list})
+    def get(self, request):
+        client_list = Client.objects.all()
+        print(client_list)
+        return render(request, 'list_client.html', context={'client_list': client_list})
--- outcrm/urls.py
+++ outcrm/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from leads.views import ClientAdd, ClientList
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('client/add/', ClientAdd.as_view()),
+    path('client/list/', ClientList.as_view())
 ]
